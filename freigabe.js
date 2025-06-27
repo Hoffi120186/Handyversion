075d9ecbe32551a7c1418999728fee3ced473467
Hoffi120186 <marc.hoffmann86@gmx.de>
2025-06-27 19:51:59 +0200

    Update freigabe.js

--- freigabe.js
+++ freigabe.js
@@ -32,11 +32,14 @@ async function prüfePasswort() {
   console.log("Passwort wird geprüft");
   const eingabe = document.getElementById("pwInput").value.trim();
 
+  const formData = new URLSearchParams();
+  formData.append("passwort", eingabe);
+
   const response = await fetch("https://www.1rettungsmittel.de/pwa-backend/check.php", {
-    method: "POST",
-    headers: {"Content-Type":"application/json"},
-    body: JSON.stringify({passwort: eingabe})
-});
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: formData.toString()
+  });
 
   const data = await response.json();
 
